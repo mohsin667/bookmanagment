3b8ad0555df083c663e4d8ec0a6f437d5c0923ad
Mohsin Ansari <ansarimohsin667@gmail.com>
2022-02-01 12:41:50 +0530

    removing lazy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState, Suspense } from "react";
+import React, { useState } from "react";
 import BookContext from "./components/BookContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.scss";
-const Edit = React.lazy(() => import("./Pages/Edit"));
-const Home = React.lazy(() => import("./Pages/Home"));
-const Registration = React.lazy(() => import("./Pages/Registration"));
-const Addbook = React.lazy(() => import("./Pages/Addbook"));
-const Booklist = React.lazy(() => import("./Pages/Booklist"));
-const Cart = React.lazy(() => import("./Pages/Cart"));
+import Edit from "./Pages/Edit";
+import Home from "./Pages/Home";
+import Registration from "./Pages/Registration";
+import Addbook from "./Pages/Addbook";
+import Booklist from "./Pages/Booklist";
+import Cart from "./Pages/Cart";
 
 function App() {
   const fetch = localStorage.getItem("cart") !== null ? JSON.parse(localStorage.getItem("cart")) : [];
@@ -18,22 +18,20 @@ function App() {
   const [library, setLibrary] = useState(retriveData);
   const data = { loggedIn, setLoggedIn, library, setLibrary, cart, setCart };
   return (
-    <Suspense fallback={<div className="loading">Loading...</div>}>
-      <Router>
-        <div className="App">
-          <BookContext.Provider value={data}>
-            <Routes>
-              <Route exact path="/" element={loggedIn === "1" ? <Home /> : <Registration />} />
+    <Router>
+      <div className="App">
+        <BookContext.Provider value={data}>
+          <Routes>
+            <Route exact path="/" element={loggedIn === "1" ? <Home /> : <Registration />} />
 
-              <Route exact path="/edit/:id" element={auth ? <Edit /> : <Registration />} />
-              <Route exact path="/addbook" element={auth ? <Addbook /> : <Registration />} />
-              <Route exact path="/booklibrary" element={auth ? <Booklist /> : <Registration />} />
-              <Route exact path="/cart" element={auth ? <Cart /> : <Registration />} />
-            </Routes>
-          </BookContext.Provider>
-        </div>
-      </Router>
-    </Suspense>
+            <Route exact path="/edit/:id" element={auth ? <Edit /> : <Registration />} />
+            <Route exact path="/addbook" element={auth ? <Addbook /> : <Registration />} />
+            <Route exact path="/booklibrary" element={auth ? <Booklist /> : <Registration />} />
+            <Route exact path="/cart" element={auth ? <Cart /> : <Registration />} />
+          </Routes>
+        </BookContext.Provider>
+      </div>
+    </Router>
   );
 }
 
